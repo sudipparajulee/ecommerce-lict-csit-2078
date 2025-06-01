9e1dc3746d79fffc312d36fbb30381584fdda0c2
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-06-01 11:15:17 +0545

    Product

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'discounted_price',
+        'stock',
+        'category_id',
+        'photopath'
+    ];
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'Products')
+@section('content')
+
+@endsection
