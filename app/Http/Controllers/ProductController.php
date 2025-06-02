1f791c62e1474b9f2d340028843fad80765c7d16
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-06-02 11:23:17 +0545

    product store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,12 +15,32 @@ class ProductController extends Controller
 
     public function create()
     {
-
+        $categories = Category::orderBy('order', 'asc')->get();
+        return view('products.create', compact('categories'));
     }
 
     public function store(Request $request)
     {
 
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'category_id' => 'required',
+            'discounted_price' => 'nullable|numeric',
+            'description' => 'required|string',
+            'stock' => 'required|numeric',
+            'photopath' => 'required|image'
+        ]);
+
+        // Handle file upload
+        $file = $request->file('photopath');
+        $fileName = time() . '.' . $file->getClientOriginalExtension();
+        $file->move(public_path('images/products'), $fileName);
+        $data['photopath'] = $fileName;
+
+        // Create the product
+        Product::create($data);
+        return redirect()->route('products.index')->with('success', 'Product Created Successfully');
     }
 
     public function edit($id)
