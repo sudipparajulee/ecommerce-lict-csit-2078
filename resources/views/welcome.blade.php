1efa43b25d20b7265a8c1e5258503c4adc306e1f
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-07-07 11:53:34 +0545

    API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,17 @@
         <h2 class="font-bold text-3xl border-l-4 border-blue-500 pl-2">Latest Products</h2>
         <div class="grid grid-cols-4 gap-4 mt-5">
             @foreach($latestproducts as $product)
-            <a href="{{route('viewproduct',$product->id)}}" class="bg-white shadow-md rounded-lg p-4 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+            @php
+                $discount_percentage = 0;
+                if($product->discounted_price != '')
+                {
+                    $discount_percentage = round((($product->price - $product->discounted_price) / $product->price) * 100);
+                }
+            @endphp
+            <a href="{{route('viewproduct',$product->id)}}" class="bg-white shadow-md rounded-lg p-4 hover:shadow-lg hover:-translate-y-2 transition-all duration-300 relative">
+                @if($discount_percentage > 0)
+                <p class="absolute right-0 bg-blue-500 text-white px-2 rounded-lg">-{{$discount_percentage}}%</p>
+                @endif
                 <img src="{{asset('images/products/'.$product->photopath)}}" alt="" class="w-full h-48 object-cover rounded-md mb-3">
                 <h3 class="font-bold text-xl">{{$product->name}}</h3>
                 <p class="text-blue-500 font-bold mt-2">
